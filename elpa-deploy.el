4ad48857651bd562aa62be2e40b8d3e9d5b4eee8
Bruno Félix Rezende Ribeiro <oitofelix@gnu.org>
2019-09-29 02:54:43 -0300

    Solve multi-file package definition parsing regression.

--- elpa-deploy.el
+++ elpa-deploy.el
@@ -124,6 +124,7 @@ Presumably file ‘*-pkg.el’ has the ‘define-package’ form as its first."
 			   (error "Package definition file ‘%s’ doesn’t exist"
 				  (f-filename pkg-file)))
     (save-excursion
+      (goto-char (point-min))
       (when (or (package-process-define-package (read (current-buffer)))
 		(error "Can’t find ‘define-package’ in %s" pkg-file))
         (goto-char (point-min))
