8b166537dbf20cb77f95142736de2e8c2f646f5b
Bruno Félix Rezende Ribeiro <oitofelix@gnu.org>
2019-10-05 20:36:15 -0300

    Update to latest oitofelix’s ELPA deployed version.

--- elpa-deploy.el
+++ elpa-deploy.el
@@ -7,7 +7,7 @@
 ;; Package: elpa-deploy
 ;; Homepage: https://github.com/oitofelix/elpa-deploy
 
-;; Version: 20190929.250
+;; Version: 20191005.2013
 ;; Package-Requires: ((emacs "24") (f "0.0"))
 
 ;; This program is free software: you can redistribute it and/or modify
